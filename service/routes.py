68e571065513a04948f40897dbb4f909dc000001
Ritika D Chandavarkar <106824256+ri-chand@users.noreply.github.com>
2024-10-17 13:17:39 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -75,10 +75,14 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+    return abort(
+        status.HTTP_404_NOT_FOUND, 
+        f"Counter {name} does not exist"
+    )
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
+
 
 
 ############################################################
@@ -90,7 +94,11 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
+
 
     COUNTER[name] += 1
 
